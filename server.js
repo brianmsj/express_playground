4bd58150859adf88635e76257498190df7c3b102
brianmsj <brianmsj@gmail.com>
2017-07-09 08:54:01 -0700

    server running

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const app = express();
 
 function logger(req,res,next) {
-  res.write(new Date())
+  console.log((new Date(), req.method, req.url));
   next()
 }
-function hello(req,res,next) {
-  res.write('Hello World')
-  next()
-}
-function goodbye(req,res,next) {
-  res.write('GoodBye World')
-  res.end()
-}
+// function hello(req,res,next) {
+//   res.write('Hello World')
+//   next()
+// }
+// function goodbye(req,res,next) {
+//   res.write('GoodBye World')
+//   res.end()
+// }
 
 app.use(logger);
-app.get('/hello',hello,goodbye)
+// app.get('/hello',hello,goodbye)
 
 
-const server = app.listen(8080)
+const server = app.listen(3000)
+console.log('server is listening')
